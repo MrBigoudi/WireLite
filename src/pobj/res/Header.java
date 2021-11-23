0e66cf3570cc6338bc705743c35c992807af179c
MrBigoudi <yannis.kedadry2@gmail.com>
2021-11-23 17:24:34 +0100

    header attribute

--- src/pobj/res/Header.java
+++ src/pobj/res/Header.java
@@ -1,5 +1,13 @@
 package pobj.res;
 
-public class Header {
+import java.util.List;
+import java.util.ArrayList;
 
+/**
+ * Classe gerant les Entetes
+ * @author Sharane et Yannis
+ *
+ */
+public class Header {
+	private List<IField> a_champs = new ArrayList<>();
 }
