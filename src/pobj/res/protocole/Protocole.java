eb69629a3e121fa04fd4608b2d16d64bff517ead
MrBigoudi <yannis.kedadry2@gmail.com>
2021-11-23 17:31:18 +0100

    debut classe protocole

--- src/pobj/res/protocole/Protocole.java
+++ src/pobj/res/protocole/Protocole.java
@@ -12,5 +12,27 @@ public abstract class Protocole implements IData {
 	//les donnes ou protocole encapsules
 	private IData data;
 	//l'entete du protocole
-	private Header head;
+	private Header header;
+	
+	/**
+	 * Constructeur prenant en argument des donnees
+	 * @param data
+	 */
+	public Protocole(IData data)
+	{
+		//TODO
+	}
+
+	/**
+	 * Accesseur sur les donnees
+	 * @return Les donnees encapsulees
+	 */
+	public IData getData() {return data;}
+	/**
+	 * Accesseur sur l'entete
+	 * @return L'entete
+	 */
+	public Header getHeader() {return header;}
+	
+	
 }
