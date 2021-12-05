78e20f02263018fa18eecbeed02f13da413c44bf
yannis <yannis@localhost>
2021-12-06 00:28:00 +0100

    squelette du main + modif gestion fichier entree

--- src/pobj/res/InputFileManager.java
+++ src/pobj/res/InputFileManager.java
@@ -1,5 +1,6 @@
 package pobj.res;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,8 +10,6 @@ import java.util.List;
  *
  */
 public class InputFileManager {
-	//le nom du fichier en entree du programme
-	private String fileName;
 	//list contenant les trames sous forme de chaines sans espaces
 	private List<String> listTrames = new ArrayList<>();
 	
@@ -18,36 +17,44 @@ public class InputFileManager {
 	 * Construteur classique
 	 * @param file Le nom du fichier en entree du programme
 	 */
-	private InputFileManager(String file)
+	public InputFileManager(String filePath)
 	{
-		//TODO cree une copie du fichier sur laquelle on travaillera
-		testCoherenceFichier();
-		removeUselessChars();
-		listTrames = this.parseTramesInFiles();
+		testCoherenceFichier(filePath);
+		removeUselessChars(filePath);
+		listTrames = this.parseTramesInFiles(filePath);
 	}
 	
 	/**
-	 * Verifie que le fichier en entree n'a pas d'erreur d'offset, une erreur d'offset implque le retrait de la trame dans le fichier
+	 * Verifie que le fichier en entree n'a pas d'erreur d'offset, une erreur d'offset implique le retrait de la trame dans le fichier
+	 * @param filePath Le fichier modifie
 	 */
-	private void testCoherenceFichier()
+	private void testCoherenceFichier(String filePath)
 	{
 		//TODO
 	}
 	
 	/**
-	 * Enleve les charcateres en trop dans le fichier d'entree
+	 * Enleve les characteres en trop dans le fichier d'entree
+	 * @param filePath Le fichier modifie
 	 */
-	private void removeUselessChars()
+	private void removeUselessChars(String filePath)
 	{
 		//TODO
 	}
 	
 	/**
 	 * Transforme chaque trames du fichier en chaine de characteres et l'ajoute dans la liste des trames
+	 * @param filePath Le fichier modifie
 	 */
-	private List<String> parseTramesInFiles()
+	private List<String> parseTramesInFiles(String filePath)
 	{
 		//TODO
 		return new ArrayList<String>();
 	}
+	
+	/**
+	 * Accesseur sur la liste des trames
+	 * @return La liste des trames
+	 */
+	public List<String> getTrames(){return this.listTrames;}
 }
