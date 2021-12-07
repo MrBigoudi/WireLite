29468aa8fa4dad857b31967e6517398d3546a3f3
yannis <yannis@localhost>
2021-12-07 10:11:39 +0100

    file read to init trames

--- tests/pobj/test/TestFileManagement.java
+++ tests/pobj/test/TestFileManagement.java
@@ -1,5 +1,7 @@
 package pobj.test;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import pobj.res.InputFileManager;
@@ -14,6 +16,9 @@ public class TestFileManagement {
 	@Test
 	public void test() {
 		InputFileManager ifm = new InputFileManager("data/traceExemple");
+		List<String> trames = ifm.getTrames();
+		for(String s : trames)
+			System.out.println(s+"\n");
 	}
 
 }
